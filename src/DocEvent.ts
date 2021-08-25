54bf13217d78df85d2f4e04f5ddca5a7b90396f8
Ben <BenjammingKirby@users.noreply.github.com>
2021-08-25 13:42:24 -0400

    Transform DocEvent

---
+++ src/DocEvent.ts
@@ -0,0 +1,17 @@
+import { DocElement } from "./DocElement";
+import { DocParam } from "./DocParam";
+import type { DocParentTypes } from "./DocBase";
+import { RawDocumentedEvent } from "./InterfacesForDocElements";
+export class DocEvent extends DocElement {
+    constructor(parent: DocParentTypes, data: RawDocumentedEvent) {
+        super(parent.doc, DocElement.types.EVENT, data, parent);
+        this.adoptAll(data.params, DocParam);
+    }
+
+    get formattedName(): string {
+        if (!this.parent) return "";
+        return `${this.parent.name}#${this.name}`;
+    }
+}
+
+module.exports = DocEvent;
